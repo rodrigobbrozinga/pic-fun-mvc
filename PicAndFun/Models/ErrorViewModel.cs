404d784a4aabad0b4a272a08e3eb9dad3a48570d
rodrigobbrozinga <rodrigobbrozinga@gmail.com>
2021-09-27 20:14:21 -0300

    Projeto criado

---
+++ PicAndFun/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PicAndFun.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
