45b0458a959d8ad15a3d13d357f5cd4e3b650c5c
rodrigobbrozinga <rodrigobbrozinga@gmail.com>
2021-09-27 21:20:13 -0300

    Página principal

--- PicAndFun/Controllers/HomeController.cs
+++ PicAndFun/Controllers/HomeController.cs
@@ -21,12 +21,7 @@ namespace PicAndFun.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        }     
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
